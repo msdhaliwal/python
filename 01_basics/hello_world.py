27da54bdd696b3c214ba27232d89c07c38e9c00f
Manpreet singh <msdhaliwal@outlook.in>
2025-02-01 20:11:29 +0530

    python chapter 1

---
+++ 01_basics/hello_world.py
@@ -0,0 +1,11 @@
+# print("Hello world!!")
+
+# def hello(n):
+#   print(n)
+
+# hello("Hello World!!")
+# hello(8)
+from printer import printer
+
+printer(3)
+printer("hi")
---
+++ 01_basics/printer.py
@@ -0,0 +1,2 @@
+def printer(n):
+  print(n)
